390cd2165ce50d7a98f359b21bc9d22d24f19c3c
KP1709 <63492101+KP1709@users.noreply.github.com>
2021-11-27 13:48:17 +0000

    Create morseCode.py

---
+++ morseCode.py
@@ -0,0 +1,41 @@
+# Morse code program (Without GUI) - Kareena Patel - 27/11/2021
+
+# Importing modules
+import winsound
+import time
+
+""" Function converts string into visual morse code"""
+def letterInMorse(string):
+    morseLetterDict = {"a":" O-", "b":"-OOO", "c":"-O-O",                 # Created dictionary to store letters in visual morse code
+                       "d":"-OO", "e":"O", "f":"OO-O",
+                       "g":"--O", "h":"OOOO", "i":"OO",
+                       "j":"O---", "k":"-O-", "l":"O-OO",
+                       "m":"--", "n":"-O", "o":"---",
+                       "p":"O--O", "q":"--O-", "r":"O-O",
+                       "s":"OOO", "t":"-", "u":"OO-",
+                       "v":"OOO-", "w":"O--", "x":"-OO-",
+                       "y":"-O--", "z":"--OO"}
+    
+    # Goes through each letter in string to convert
+    global conversion                                                     # Creating a global variable (used in SoundInMorse)
+    conversion = ""
+    for letter in string:
+        x = morseLetterDict.get(letter)
+        conversion = conversion + "|" + x                                 #'|' - characters are readable and concatenate
+    return conversion
+
+""" Function converts visual morse code into sound"""
+def morseInSound(conversion):
+    sep = conversion.replace("|", " ")                                    # Changes '|' into spaces for conversion
+    print(sep)
+    for char in sep:
+        if char == "O":
+            winsound.Beep(500,100)                                        # Uses system bell at set frequency and duration (depending on char)
+        elif char == "-":
+            winsound.Beep(500,300)
+        else:
+            time.sleep(2)                                                 # Whitespace shown by pause (sleep) 
+    
+string = str.lower(input("Enter a string to convert to morse: "))
+letterInMorse(string)
+morseInSound(conversion)
